b67f64b9c46b82861853ad9e9184c718affbfedc
Yuki03280322 <72023246+Yuki03280322@users.noreply.github.com>
2020-10-20 12:15:53 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "messages#index"#これを一番最初に見てほしいと指定している
+  root to: "rooms#index"#これを一番最初に見てほしいと指定している
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
