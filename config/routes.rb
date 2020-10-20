87a43676f6c272bd1f5359f2805da9577bdde9fc
Yuki03280322 <72023246+Yuki03280322@users.noreply.github.com>
2020-10-20 09:20:42 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"#これを一番最初に見てほしいと指定している
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
